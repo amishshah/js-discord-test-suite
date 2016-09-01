c8438d4c6fcef4791e309c3404370642eb548070
Amish Shah <amishshah.2k@gmail.com>
2016-09-01 23:10:05 +0100

    add ram sending

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const childProcess = require('child_process');
+const express = require('express');
+const eventsource = require('express-eventsource');
+const cors = require('cors');
+
+const sse = eventsource({
+  connections: 2,
+});
+
+const sendMessage = sse.sender('message');
+
+const app = express()
+  .use(cors())
+  .use(sse.middleware())
+  .listen(3000);
 
 function spawnTest(file) {
   const proc = childProcess.fork(file, null, console.log);
   const lib = file.split('/')[2];
   proc.on('message', m => {
-    console.log(lib, m);
+    sendMessage({
+      lib,
+      t: 'ram',
+      d: m.d,
+    });
   });
 }
 
